df5dc3c0c7213207e01b2ceb34f65c8ec964d14f
JBL-987 <jasonbrandonloi03@gmail.com>
2025-03-16 21:53:44 +0700

    cape gw2

--- js/cinema.js
+++ js/cinema.js
@@ -1,14 +1,28 @@
 let swiper = new Swiper(".mySwiper", {
-      slidesPerView: 5,
-      spaceBetween: 2,
+      slidesPerView: 1,
+      spaceBetween: 30,
       loop: true,
       pagination: {
         el: ".swiper-pagination",
         clickable: true,
       },
-      navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
+       breakpoints: {
+        480: {
+            slidesPerView: 2,
+            spaceBetween: 15,
+        },
+        768: {
+            slidesPerView: 3,
+            spaceBetween: 20,
+        },
+        1024: {
+            slidesPerView: 4,
+            spaceBetween: 25,
+        },
+        1200: {
+            slidesPerView: 5,
+            spaceBetween: 30,
+        },
     },
 });
 
